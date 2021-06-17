93e1f584605517b260241bde142ad31fbca3f734
vstanek <vstanek@redhat.com>
2021-06-17 14:40:46 +0200

    Add hardware and OS info to the script

--- gnome-info-collect.sh
+++ gnome-info-collect.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
 {
+#~ Get computer manufacturer, model and running operating system 
+#~ including its version
+
+hostnamectl | grep -e 'Hardware' -e 'Operating System'
+
 #~ Get list of all installed apps
 
 gdbus call --session --dest org.gnome.Shell --object-path /org/gnome/Shell --method org.gnome.Shell.Eval 'imports.gi.Shell.AppSystem.get_default().get_installed().filter(app => imports.misc.parentalControlsManager.getDefault().shouldShowApp(app)).map(app => app.get_id())'
