b497b1bfda05ced33121ad73e50e839233f72aaf
vstanek <vstanek@redhat.com>
2021-06-24 14:57:27 +0200

    Touches on the commands and added error handling

--- gnome-info-collect.sh
+++ gnome-info-collect.sh
@@ -12,31 +12,38 @@ hostnamectl | grep -e 'Hardware' -e 'Operating System' | sed -e 's/^[ \t]*/"/g' 
 
 #~ Get list of all installed apps
 
+installed_apps=$(gdbus call --session --dest org.gnome.Shell --object-path /org/gnome/Shell --method org.gnome.Shell.Eval 'imports.gi.Shell.AppSystem.get_default().get_installed().filter(app => imports.misc.parentalControlsManager.getDefault().shouldShowApp(app)).map(app => app.get_id())' || echo '"Error"')
+
 echo -n '"Installed apps":'
-gdbus call --session --dest org.gnome.Shell --object-path /org/gnome/Shell --method org.gnome.Shell.Eval 'imports.gi.Shell.AppSystem.get_default().get_installed().filter(app => imports.misc.parentalControlsManager.getDefault().shouldShowApp(app)).map(app => app.get_id())' | grep -o '\[.*]' | sed -e 's/.desktop//g' -e 's/]/],/'
+if [ "$installed_apps" != '"Error",' ]
+    then
+        echo $installed_apps | grep -o '\[.*]' | sed -e 's/.desktop//g' -e 's/]/],/'
+    else
+        echo $installed_apps
+fi
 
-#~ Get list of GNOME favorited apps
+#~ Get list of favorited apps (in Dash)
 
-favs=$(gsettings get org.gnome.shell favorite-apps | sed "s/'/\"/g" )
+favs=$(gsettings get org.gnome.shell favorite-apps || echo '"Error",')
 
 echo -n '"Favourited apps":'
 echo $favs | \
-if grep -q "@as \[\]" 
+if grep -q "@as \[\]"
     then 
         echo '"None"' 
     else 
-        echo "$favs,"
+        echo "$favs," | sed -e "s/'/\"/g" -e "s/.desktop//g" # Substitute " for ' and remove .desktop
 fi
 
 
 #~ Get worspaces only on primary display
 
-workspaces_primary=$(gsettings get org.gnome.mutter workspaces-only-on-primary || echo '"Error"')
+workspaces_primary=$(gsettings get org.gnome.mutter workspaces-only-on-primary || echo '"Error",')
 echo "\"Workspaces only on primary\":$workspaces_primary,"
 
 #~ Get wheter workspaces are dynamic
 
-workspaces_dynamic=$(gsettings get org.gnome.shell.overrides dynamic-workspaces || echo '"Error"')
+workspaces_dynamic=$(gsettings get org.gnome.shell.overrides dynamic-workspaces || echo '"Error",')
 echo "\"Workspaces dynamic\":$workspaces_dynamic,"
 
 #~ Get list of enabled GNOME extensions
