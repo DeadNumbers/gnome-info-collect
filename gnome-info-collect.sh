50063d1e683233d764ecf8653d82936145b8afba
vstanek <vstanek@redhat.com>
2021-09-22 15:13:18 +0200

    Flatpak install and flathub enabled checks added

--- gnome-info-collect.sh
+++ gnome-info-collect.sh
@@ -7,11 +7,30 @@ echo "{"
     
 #~ Get computer manufacturer, model and running operating system 
 #~ including its version
+hostnamectl | grep -e 'Hardware' -e 'Operating System' | sed -e 's/^[ \t]*/"/g' -e 's/:[ \t]*/":"/g' -e 's/$/",/g' # Convert to JSON format using sed
 
-hostnamectl | grep -e 'Hardware' -e 'Operating System' | sed -e 's/^[ \t]*/"/g' -e 's/:[ \t]*/":"/g' -e 's/$/",/g' #Convert to JSON format using sed
+#~ Check if flatpak is installed
+flatpak 2>/dev/null
+retval=$?
+if [ $retval -eq 127 ]
+    then 
+        #~ Flatpak is not installed
+        echo "\"Flatpak installed\":false,"
+        echo "\"Flathub enabled\":false,"
+    else
+        #~ Flatpak is installed
+        echo "\"Flatpak installed\":true,"
+        
+        #~ Check if flathub is enabled
+        if flatpak remotes --columns url | grep 'https://dl.flathub.org/repo/' >/dev/null
+            then
+                echo "\"Flathub enabled\":true,"
+            else
+                echo "\"Flathub enabled\":false,"
+        fi
+fi
 
 #~ Get list of all installed apps
-
 installed_apps=$(gdbus call --session --dest org.gnome.Shell --object-path /org/gnome/Shell --method org.gnome.Shell.Eval 'imports.gi.Shell.AppSystem.get_default().get_installed().filter(app => imports.misc.parentalControlsManager.getDefault().shouldShowApp(app)).map(app => app.get_id())' || echo '"Error"')
 
 echo -n '"Installed apps":'
@@ -22,8 +41,7 @@ if [ "$installed_apps" != '"Error",' ]
         echo $installed_apps
 fi
 
-#~ Get list of favorited apps (in Dash)
-
+#~ Get list of favorited apps (in GNOME Dash)
 favs=$(gsettings get org.gnome.shell favorite-apps || echo '"Error",')
 
 echo -n '"Favourited apps":'
@@ -37,19 +55,16 @@ fi
 
 
 #~ Get worspaces only on primary display
-
 workspaces_primary=$(gsettings get org.gnome.mutter workspaces-only-on-primary || echo '"Error",')
 echo "\"Workspaces only on primary\":$workspaces_primary,"
 
 #~ Get wheter workspaces are dynamic
-
 workspaces_dynamic=$(gsettings get org.gnome.shell.overrides dynamic-workspaces || echo '"Error",')
 echo "\"Workspaces dynamic\":$workspaces_dynamic,"
 
 #~ Get list of enabled GNOME extensions
-
 extensions=$(gnome-extensions list --enabled | sed -e 's/^/"/g' -e 's/$/",/g')
-echo "\"Enabled extensions\":[$extensions]" | sed 's/,]/]/' #Don't forget to add a comma after the array if more commands follow after this one!
+echo "\"Enabled extensions\":[$extensions]" | sed 's/,]/]/' # Don't forget to add a comma after the array if more commands follow after this one!
 
 #~ End JSON object
 echo "}"
