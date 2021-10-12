b11a41a4e585eb0edff8afa73a0150a49dc8d8a9
vstanek <vstanek@redhat.com>
2021-10-12 12:54:18 +0200

    Implemented basic server

--- server/server.py
+++ server/server.py
@@ -1,6 +1,32 @@
 #!/usr/bin/env python
 
+from http.server import ThreadingHTTPServer, BaseHTTPRequestHandler
+
+class RequestHandler(BaseHTTPRequestHandler):
+    def _set_response(self):
+        self.send_response(200)
+        self.send_header('content-type', 'text/html')
+        self.end_headers()
+    
+    def do_GET(self):
+        self._set_response()
+        self.wfile.write("Hello world!".encode())
+
+    def do_POST(self):
+        length = int(self.headers['Content-Length']) # Gets the size of data
+        data = self.rfile.read(length) # Gets the data
+        
+        # ~ Change to save the data into a file
+        print(f"Recieved data: {data.decode()}")
+        
+        self._set_response()
+        self.wfile.write("Request recieved\n".encode())
+
+def main():
+    PORT = 12345
+    server = ThreadingHTTPServer(('', PORT), RequestHandler)
+    print(f"Server running on port {PORT}")
+    server.serve_forever()
+
 if __name__ == '__main__':
-    print("Server running.")
-    while True:
-        pass
+    main()
