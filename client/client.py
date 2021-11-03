77494629b6417c79c723b5f2fb3622df63c2fd3c
vstanek <vstanek@redhat.com>
2021-11-03 22:18:22 +0100

    Removed trailing import, added exit codes and permission check for executing gnome-info-collect.sh

--- client/client.py
+++ client/client.py
@@ -9,18 +9,19 @@
 import subprocess
 import requests
 import os
-import js2py
 
 # ~ Address of a server to send the data to
 ADDRESS="https://gnome-info-collect-gnome-info-collect.openshift.gnome.org"
 
-# add check for permissions with PermissionError
 # ~ Run the script and get the info
 try:
     json_output = subprocess.run(os.path.dirname(os.path.abspath(__file__)) + "/gnome-info-collect.sh", shell=False, capture_output=True, check=True).stdout
 except subprocess.CalledProcessError as e:
     print(f"Error collecting the data!\nExit code: {e.returncode}\nOuptut: {e.output}")
     exit(e.returncode)
+except PermissionError:
+    print(f"Error collecting data: permission denied.")
+    exit(9)
 
 # ~ Debug 
 # ~ print(f"Output:\n{json_output}\n")
@@ -34,12 +35,16 @@ try:
     
 except requests.HTTPError:
     print(f"Status {r.status_code}: An HTTP error occured\nServer message: {r.text}\n")
+    exit(10)
 except requests.ConnectionError:
     print("Connection Error: Please check your internet connection and try again\n")
+    exit(11)
 except requests.Timeout:
     print("Timeout error: Request timed out\nPlease check your internet connection and try again\n")
+    exit(12)
 except:
     print("Unknown error: Sending data unsuccessful, please, try again.\n")
+    exit(13)
 
 else:
     # ~ No errors, print server output
