a109aec2a5ce5615f375e6f362bd9d9e142892b3
vstanek <vstanek@redhat.com>
2021-12-02 15:40:20 +0100

    Prevent double sending in client

--- client/client.py
+++ client/client.py
@@ -11,16 +11,28 @@ import requests
 import os
 import json
 
+from gi.repository import GLib
+
 # ~ Address of a server to send the data to
 ADDRESS = "https://gnome-info-collect-gnome-info-collect.openshift.gnome.org"
 # ~ Max length of a key in json for pretty formatted output
 MAX_LEN = 26 # "Workspaces only on primary"
+# ~ User application data directory and status file
+USER_DIR = GLib.get_user_data_dir()
+APP_DIR = USER_DIR + '/gnome-info-collect'
+STATUS_FILE = APP_DIR + '/uploaded'
+
+
+# ~ Check if status file exists (if data was already once successfully uploaded) and exit if yes
+if os.path.isfile(STATUS_FILE):
+    print("Information was already successfuly uploaded.\nNot collecting or sending any data, exiting...")
+    exit(0)
 
 # ~ Run the script and get the info
 try:
     json_output = subprocess.run(os.path.dirname(os.path.abspath(__file__)) + "/gnome-info-collect.sh", shell=False, capture_output=True, check=True).stdout
 except subprocess.CalledProcessError as e:
-    print(f"Error collecting the data!\nExit code: {e.returncode}\nOuptut: {e.output}")
+    print(f"Error collecting data!\nExit code: {e.returncode}\nOuptut: {e.output}")
     exit(e.returncode)
 except PermissionError:
     print(f"Error collecting data: permission denied.")
@@ -41,7 +53,12 @@ print("The following information will be sent to the GNOME project:\n")
 for key, value in data.items():
     if key in ('Installed apps', 'Favourited apps', 'Online accounts', 'Enabled extensions'): # Value is an array
         print(f"**{key}**")
-        print(*("'{}'".format(v) for v in value), sep=", ") # unpack the array and print ' around
+        if value == '"Error"': # Error collecting this specific data
+            print(f"Error collecting {key}")
+        if not value: # Empty array
+            print("None")
+        else:
+            print(*("'{}'".format(v) for v in value), sep=", ") # unpack the array and print ' around
     else:
         print(f"**{key}**{(MAX_LEN-len(key)+4)*' '}{value}")
 
@@ -53,6 +70,7 @@ try:
     c = input()
 
     if c in ('n', 'N', ''):
+        print("Exiting...")
         exit(0)
     elif c not in ('y', 'Y'):
         raise ValueError
@@ -87,8 +105,15 @@ except requests.Timeout:
 except:
     print("Unknown error: Sending data unsuccessful, please, try again.\n")
     exit(23)
-else:
-    # ~ No errors, print server output
-    print(f"Status {r.status_code}: {r.text}")
 
+# ~ No errors, print server output
+print(f"Status {r.status_code}: {r.text}")
+
+# ~ Create a file in user app dir to preven user from uploading the data multiple times
+if not os.path.isdir(APP_DIR): # create app dir if doesn't exist
+    os.mkdir(APP_DIR)
+with open(STATUS_FILE, 'x') as f:
+    f.write('{"status": "successful"}\n')
+
+# ~ Finish
 print("Complete! Thank you for helping to improve GNOME.")
